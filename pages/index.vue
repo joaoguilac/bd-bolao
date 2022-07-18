b97660f44993b21c1d25928193a2faa921def6c2
CarolPatricio <carolpatricio28@hotmail.com>
2022-07-18 01:55:37 -0300

    Estilizações da tela inicial e login

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,150 @@
+
 <template>
-  <b-container>
-    <img src="bolao-logo.svg" alt="">
-    <h1>Para você que gosta de futebol e de apostas</h1>
-    <p>Faça o seu cadastro e chame seus amigos</p>
-    <br>
-    <nuxt-link to="/login">
-      <b-button>Login</b-button>
-    </nuxt-link>
-    <br><br>
-    <form @submit.prevent="login">
-      <input
-        required
-        type="email"
-        v-model="email"
-        class="form-control"
-        placeholder="E-mail"
-      />
-      <b-button type="submit" block variant="success" class="px-4">
-        Cadastrar
-      </b-button>
-    </form>
-    <br><br>
-    <img src="smartphone-index.svg" alt="">
-    <h1>Se você quer se divertir apostando em jogos de futebol, bolão é pra você!</h1>
-  </b-container>
+  <div>
+    <div class="border d-flex w-100">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <b-container>
+      <div class="w-100 d-flex align-items-center justify-content-center mt-5 mb-3">
+        <img src="bolao-logo.svg" alt="Logo do Bolão" style="width:20rem;" />
+      </div>
+      <div class="info w-100 d-flex align-items-center justify-content-center flex-column my-5">
+        <h1 class="mb-3">Para você que gosta de futebol e de apostas</h1>
+        <p>Faça o seu cadastro e chame seus amigos</p>
+      </div>
+      <form @submit.prevent="login" style="width: 100%;" class="w-100 d-flex align-content-center justify-content-between">
+        <input
+          required
+          type="email"
+          v-model="email"
+          class="form-control mr-3"
+          placeholder="E-mail"
+        />
+        <input
+          required
+          type="password"
+          v-model="senha"
+          class="form-control mr-3"
+          placeholder="Senha"
+        />
+        <b-button type="submit" block variant="success" class="px-4 w-25">
+          Cadastrar
+        </b-button>
+      </form>
+      <img src="smartphone-index.svg" alt="" />
+    </b-container>
+    <div class="login-box back-green w-100">
+      <h2>
+        Se você quer se divertir apostando em jogos de futebol, bolão é pra você!
+      </h2>
+      <p>Faça o seu login</p>
+      <nuxt-link to="/login" class="w-25">
+        <b-button type="submit" block variant="success">Login</b-button>
+      </nuxt-link>
+    </div>
+    <div class="footer back-green w-100">
+      <p class="ml-4">Ana Carolina | Cácio Lucas | João Guilherme </p>
+      <p>© 2022 - Todos os direitos reservados</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return {
+      email: "",
+      senha: "",
+    };
+  },
+};
 </script>
+
+<style>
+
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap');
+
+  :root {
+    --primary-color: #13c75f;
+    --text-color: #7e7e90;
+    --sub-text-color: #ababc0;
+    --second-color: #f2c94c;
+    --danger-color: #EB5757;
+    --details-color: #6a6180;
+  }
+
+  * {
+    margin: 0;
+    text-decoration: none;
+    font-family: 'Poppins', sans-serif;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,body{
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    position: relative;
+    overflow-x: hidden;
+    color: white;
+    background-color: #F9F8F4;
+  }
+
+</style>
 
 <style scoped>
 
+.border > span {
+  width:30%;
+  height: 8px;
+  display: block;
+}
+.border span:nth-child(1n), span:nth-child(4n){
+  background-color: var(--second-color);
+}
+.border span:nth-child(2n), span:nth-child(5n) {
+  background-color: var(--primary-color);
+}
+.border span:nth-child(3n), span:nth-child(6n) {
+  background-color: var(--danger-color);
+}
+
+.info{
+  color: var(--text-color);
+}
+
+.info h1{
+  font-weight: 300;
+}
+
+.login-box{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10rem;
+}
+
+.login-box p{
+  font-size: 1.3rem;
+}
+
+.back-green {
+  background-color: var(--primary-color);
+}
+.footer{
+  display: grid;
+  grid-template-columns: 1fr 20rem 1fr;
+  margin: 0 auto;
+  padding: 1rem;
+}
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,60 +1,67 @@
 <template>
-  <b-container>
-    <img src="bolao-logo-white.svg" alt="">
-    <h1>LOGIN</h1>
-    <p>Faça o login para começar a apostar</p>
-    <br>
-    <form @submit.prevent="login">
-      <input
-        required
-        type="email"
-        v-model="email"
-        class="form-control"
-        placeholder="E-mail"
-      />
-      <br>
-      <input
-        id="password"
-        required
-        :type="passwordFieldType"
-        v-model="senha"
-        class="form-control"
-        placeholder="Senha"
-      />
-      <b-row class="px-0">
-        <b-col cols="6">
-          <b-form-checkbox
-            id="checkbox-1"
-            class="mt-2 ml-2 text"
-            v-model="mostrarSenha"
-            @change="mudarVisibilidadeSenha"
-          >
-            Mostrar a senha
-          </b-form-checkbox>
-        </b-col>
-        <b-col cols="6" class="text-right mb-2">
-          <b-button to="/esqueciSenha" variant="link">Esqueceu sua senha?</b-button>
-        </b-col>
-      </b-row>
-      <b-button type="submit" block variant="success" class="px-4">
-        Entrar
-      </b-button>
-      <!-- <b-row>
-        <b-col>
-          <p class="mt-3 mb-3 text-center">
-            Ainda não tem conta?
-            <nuxt-link :to="`/register`">Cadastrar-se</nuxt-link>
-          </p>
-        </b-col>
-      </b-row> -->
-    </form>
-    <br>
-    <nuxt-link to="/usuario">
-      <b-button>
-        Entrar
-      </b-button>
-    </nuxt-link>
-  </b-container>
+  <div>
+    <div class="border d-flex w-100">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <b-container>
+      <div class="login-box d-flex flex-column align-items-center justify-content-center">
+        <img src="bolao-logo-white.svg" alt="Logo Bolão" style="width: 18rem;">
+        <div class="w-100 d-flex flex-column justify-content-lg-start align-content-center mb-4">
+          <h1 id="title-login">> LOGIN</h1>
+          <p>Faça o login para começar a apostar</p>
+        </div>
+        <form @submit.prevent="login">
+          <input
+            required
+            type="email"
+            v-model="email"
+            class="form-control"
+            placeholder="E-mail"
+          />
+          <br>
+          <input
+            id="password"
+            required
+            :type="passwordFieldType"
+            v-model="senha"
+            class="form-control"
+            placeholder="Senha"
+          />
+          <b-row class="px-0">
+            <b-col cols="6">
+              <b-form-checkbox
+                id="checkbox-1"
+                class="mt-2 ml-2 text"
+                v-model="mostrarSenha"
+                @change="mudarVisibilidadeSenha"
+              >
+                Mostrar a senha
+              </b-form-checkbox>
+            </b-col>
+            <b-col cols="6" class="text-right mb-2">
+              <b-button to="/esqueciSenha" variant="link">Esqueceu sua senha?</b-button>
+            </b-col>
+          </b-row>
+          <b-button type="submit" block variant="success" class="px-4">
+            Entrar
+          </b-button>
+          <!-- <b-row>
+            <b-col>
+              <p class="mt-3 mb-3 text-center">
+                Ainda não tem conta?
+                <nuxt-link :to="`/register`">Cadastrar-se</nuxt-link>
+              </p>
+            </b-col>
+          </b-row> -->
+        </form>
+      </div>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -127,5 +134,74 @@ export default {
 </script>
 
 <style>
+
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap');
+
+  :root {
+    --primary-color: #13c75f;
+    --text-color: #7e7e90;
+    --sub-text-color: #ababc0;
+    --second-color: #f2c94c;
+    --danger-color: #EB5757;
+    --details-color: #6a6180;
+  }
+
+  * {
+    margin: 0;
+    text-decoration: none;
+    font-family: 'Poppins', sans-serif;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,body{
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    position: relative;
+    overflow-x: hidden;
+    color: white;
+    background-color: #F9F8F4;
+  }
+
+</style>
+
+
+<style scoped>
+
+.border > span {
+  width:30%;
+  height: 8px;
+  display: block;
+}
+.border span:nth-child(1n), span:nth-child(4n){
+  background-color: var(--second-color);
+}
+.border span:nth-child(2n), span:nth-child(5n) {
+  background-color: var(--primary-color);
+}
+.border span:nth-child(3n), span:nth-child(6n) {
+  background-color: var(--danger-color);
+}
+
+.container{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-box{
+  background-color: #D8D8D8;
+  border: 1px solid #D8D8D8;
+  border-radius: 10px;
+  padding: 5rem;
+}
+
+#title-login{
+  font-size:1.7rem;
+}
 
 </style>
